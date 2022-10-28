dc4ef754741331a8fd0ed3f64e7dc9bbaba9feef
vbird <841952496@qq.com>
2022-10-28 16:29:47 +0800

    test: TestSingleGoroutine 单协程测试

--- examples/main_test.go
+++ examples/main_test.go
@@ -1,7 +1,43 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"github.com/chalvern/gochan"
+	"go.uber.org/zap"
+	"testing"
+	"time"
+)
 
 func TestMyMain(t *testing.T) {
 	main()
+}
+
+func TestSingleGoroutine(t *testing.T) {
+	logger, _ := zap.NewDevelopment()
+	sugar := logger.Sugar()
+	gochan.SetLogger(sugar)
+
+	gochanNum := 1
+	bufferNum := 10
+	manager := Manager{
+		gochanNum:  gochanNum,
+		bufferNum:  bufferNum,
+		dispatcher: gochan.NewDispatcher(gochanNum, bufferNum),
+	}
+
+	//objID := 1
+	var myNumber int = 0
+	for i := 0; i < 100000; i++ {
+		task1 := func() error {
+			myNumber++
+			return nil
+		}
+		manager.Dispatch(-1, task1)
+	}
+
+	time.Sleep(time.Second * 2)
+	if myNumber != 100000 {
+		fmt.Printf("myNumber:%d\n", myNumber)
+		panic("myNumber should be 1000")
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/chalvern/gochan
 go 1.12
 
 require (
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/stretchr/testify v1.3.0
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
